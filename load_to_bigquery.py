2c6f197ef60d39c43350f7a805a2d8c4ab57ecad
CodeHub-CloudSeals <codehub@cloudseals.com>
2025-07-27 18:47:56 +0530

    load_to_bigquery.py

--- load_to_bigquery.py
+++ load_to_bigquery.py
@@ -1,17 +1,13 @@
+import os
 from google.cloud import bigquery
 import pandas as pd
 
-# Authenticate using service account JSON
-client = bigquery.Client.from_service_account_json("service-account.json")
+client = bigquery.Client.from_service_account_json(os.environ["GOOGLE_APPLICATION_CREDENTIALS"])
 
-# Load CSV
 df = pd.read_csv("grafana_metrics.csv")
-
-# Define your table destination: project_id.dataset.table_name
 table_id = "observability-459214.monitoring.grafana_metrics"
 
-# Load to BigQuery
 job = client.load_table_from_dataframe(df, table_id)
-job.result()  # Wait for the job to complete
+job.result()
 
 print("✅ Data uploaded to BigQuery")
