2f3d1fda93f7c6ce0ccf91d0c7e2232fde0cced0
CodeHub-CloudSeals <codehub@cloudseals.com>
2025-07-28 22:24:24 +0530

    load_to_bigquery.py

--- load_to_bigquery.py
+++ load_to_bigquery.py
@@ -1,15 +1,15 @@
 import pandas as pd
 from google.cloud import bigquery
- 
+
 df = pd.read_csv("grafana_metrics.csv")
 df['timestamp'] = pd.to_datetime(df['timestamp'], errors='coerce')
- 
+
 project_id = "observability-459214"
 dataset_id = "monitoring"
 table_id = f"{project_id}.{dataset_id}.grafana_metrics"
- 
+
 client = bigquery.Client()
- 
+
 schema = [
     bigquery.SchemaField("timestamp", "TIMESTAMP"),
     bigquery.SchemaField("instance_id", "STRING"),
@@ -26,7 +26,7 @@ schema = [
     bigquery.SchemaField("catalog_disk_io", "INTEGER"),
     bigquery.SchemaField("recommendation", "STRING"),
 ]
- 
+
 try:
     client.get_table(table_id)
     print(f"Table {table_id} exists.")
@@ -35,7 +35,8 @@ except:
     table = bigquery.Table(table_id, schema=schema)
     table = client.create_table(table)
     print(f"Created table: {table.table_id}")
- 
-job = client.load_table_from_dataframe(df, table_id)
+
+job_config = bigquery.LoadJobConfig(autodetect=True)
+job = client.load_table_from_dataframe(df, table_id, job_config=job_config)
 job.result()
 print("Data loaded successfully into BigQuery.")
